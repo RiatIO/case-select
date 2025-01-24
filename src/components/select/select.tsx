dea61f311507c6e9330b1def5ad4929e696874c0
Riat <sinriat@gmail.com>
2025-01-25 00:07:50 +0100

    chore: move select component

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -8,7 +8,7 @@ import {
   useRef,
   useState,
 } from "react";
-import { useClickOutsideListener } from "../hooks";
+import { useClickOutsideListener } from "../../hooks";
 
 interface SelectContext {
   open: boolean;
