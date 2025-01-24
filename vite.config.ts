b376bfd6cb9fcb6ea1921db81faa9706005b2f58
Riat <sinriat@gmail.com>
2025-01-24 20:51:40 +0100

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Enables global test APIs like `describe` and `it`
+    environment: 'jsdom', // Simulates a browser-like environment
+    setupFiles: './src/setupTests.ts', // Path to the setup file
+  },
 })
